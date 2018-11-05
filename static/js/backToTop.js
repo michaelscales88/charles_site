64123d954aaeef35209f945903c9aabe514cafc3
Michael Scales <mhs42@txstate.edu>
2018-11-05 15:00:38 -0600

    updated js scrolling

--- static/js/backToTop.js
+++ static/js/backToTop.js
@@ -1,14 +1,19 @@
-let btn = $('a#topButton');
+let $btn = $('a#topButton');
+let $winCached = $(window),
+    $docCached = $(document);
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-        btn.addClass('show');
+$winCached.scroll(function () {
+    console.log($winCached.scrollTop() + $winCached.height());
+    console.log($docCached.height() - 200);
+    console.log($winCached.scrollTop() + $winCached.height() > $docCached.height() - 200);
+    if ($winCached.scrollTop() + $winCached.height() > $docCached.height() - 200) {
+        $btn.show();
     } else {
-        btn.removeClass('show');
+        $btn.hide();
     }
 });
 
-btn.on('click', function (e) {
+$btn.on('click', function (e) {
     e.preventDefault();
     $('html, body').animate({scrollTop: 0}, '300');
 });
